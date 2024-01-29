504b8647639cf0417e7c17a11faa87f0e1cac95f
GiPisano <pisanogiacomo9@gmail.com>
2024-01-29 16:12:59 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+const { createApp } = Vue;
+
+createApp({
+data() {
+    return {
+    message: 'Hello World!',
+    image: 'https://austingil.com/wp-content/uploads/Vue-Blog-Cover.svg'
+    }
+}
+}).mount('#app')
+
